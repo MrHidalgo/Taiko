f89a0982b9ac0b9346838c00ab7ac810cda2ddfa
Sergey Khmelevskoy <xs290@me.com>
2018-03-22 22:52:41 +0300

    removed lazy

--- src/js/main.js
+++ src/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     initSliders();
     initScrollMonitor();
     initMasks();
-    initLazyLoad();
+    // initLazyLoad();
 
     // development helper
     _window.on('resize', debounce(setBreakpoint, 200))
@@ -397,30 +397,7 @@ $(document).ready(function(){
     });
 
   }
-
-
-  //////////
-  // LAZY LOAD
-  //////////
-  function initLazyLoad(){
-    _document.find('[js-lazy]').Lazy({
-      threshold: 500,
-      enableThrottle: true,
-      throttle: 100,
-      scrollDirection: 'vertical',
-      effect: 'fadeIn',
-      effectTime: 350,
-      // visibleOnly: true,
-      // placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
-      onError: function(element) {
-          console.log('error loading ' + element.data('src'));
-      },
-      beforeLoad: function(element){
-        // element.attr('style', '')
-      }
-    });
-  }
-
+  
   //////////
   // BARBA PJAX
   //////////
